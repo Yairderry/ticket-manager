a815d73e3c6825507e760aa2b1dc01e963b31f1e
Yairderry <77237113+Yairderry@users.noreply.github.com>
2021-03-23 14:02:12 +0200

    Added 2 PATCH entrypoints to update done property in ticket

--- app.js
+++ app.js
@@ -5,11 +5,42 @@ const app = express();
 
 app.use(express.static("client/build"));
 
+// GET methods
 app.get("/api/tickets", (req, res) => {
   const { searchText } = req.query;
   Ticket.find({ title: new RegExp(searchText, "gi") }).then((data) => {
     res.json(data);
   });
+});
+
+// PATCH methods
+app.patch("/api/tickets/:ticketId/done", (req, res) => {
+  const { ticketId } = req.params;
+  console.log(ticketId);
+  Ticket.findByIdAndUpdate(ticketId, { $set: { done: true } }, { new: true })
+    .then((data) => {
+      if (!data) return res.status(404).json({ error: "Ticket Not Found" });
+
+      return res.json({ updated: true });
+    })
+    .catch((err) => {
+      if (err.name === "CastError")
+        res.status(400).json({ error: err.message });
+    });
+});
+
+app.patch("/api/tickets/:ticketId/undone", (req, res) => {
+  const { ticketId } = req.params;
+  Ticket.findByIdAndUpdate(ticketId, { $set: { done: false } }, { new: true })
+    .then((data) => {
+      if (!data) return res.status(404).json({ error: "Ticket Not Found" });
+
+      return res.json({ updated: true });
+    })
+    .catch((err) => {
+      if (err.name === "CastError")
+        res.status(400).json({ error: err.message });
+    });
 });
 
 module.exports = app;
